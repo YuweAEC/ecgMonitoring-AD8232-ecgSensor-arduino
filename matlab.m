fe5e25bf263b9fbc7200a3f87b6f6d7f0f18c6f4
Yuvraj Singh <yuwe18b@gmail.com>
2024-03-14 23:53:51 +0530

    feat:add new matlab file for area

---
+++ matlab.m
@@ -0,0 +1,11 @@
+% Program to calculate the area of a rectangle
+
+% Input the length and width of the rectangle
+length = input('Enter the length of the rectangle: ');
+width = input('Enter the width of the rectangle: ');
+
+% Calculate the area of the rectangle
+area = length * width;
+
+% Display the result
+fprintf('The area of the rectangle is: %.2f\n', area);zz
